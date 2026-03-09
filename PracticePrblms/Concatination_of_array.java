cb6fc4b07b32fab30088388dc9007ec965184b75
darshan75462020-eng <darshan75462020@gmail.com>
2026-03-09 22:25:19 +0530

    Convert PracticePrblms from submodule to normal folder

---
+++ PracticePrblms/Concatination_of_array.java
@@ -0,0 +1,14 @@
+public class Concatination_of_array {
+    
+    public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int[] ans= new int[2*n];
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        return ans;
+    }
+}
+
